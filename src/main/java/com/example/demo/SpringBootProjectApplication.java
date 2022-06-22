438615e3510b5a2607b502b28c0acf4eca9773a0
prakash <pktpune@gmail.com>
2022-06-23 00:00:21 +0530

    test

---
+++ src/main/java/com/example/demo/SpringBootProjectApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(SpringBootProjectApplication.class, args);
+
+		System.out.println("Helow World");
+	}
+
+}
